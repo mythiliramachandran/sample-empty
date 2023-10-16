17d18c011897d267c7ac1f39ca7ef7d9a266f1a8
user <mythiliramachandran@gmail.com>
2023-10-16 11:24:08 +0000

    Initializing the project

---
+++ src/test/java/com/example/sampledemo/SampledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
